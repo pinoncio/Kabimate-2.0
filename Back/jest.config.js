244d490e85ce3ba16c14bb9a20839ead6d86306f
NicolasPavz <npavez43@gmail.com>
2024-11-21 18:42:05 -0300

    retroseso commit en el back

--- Back/jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/tests/**/*.test.ts'],
-    testPathIgnorePatterns: ['<rootDir>/src/index.ts'] // Carpeta donde estan los test
-};
