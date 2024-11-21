7dd6e9ab6d27764e1cc94b6efcfa46eadfa83208
NicolasPavz <npavez43@gmail.com>
2024-11-21 15:21:33 -0300

    tests

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,8 @@
+describe('Prueba Inicio de Sesion', () => {
+  it('Inicio de sesíon correctamente',() => {
+    cy.visit('http://localhost:3000/')
+      cy.get('input[name="email"]').type('usuario@pruebas.cl')
+      cy.get('input[name="contraseña"]').type('pruebas')
+      cy.get('#boton').click()
+  })
+})
