0613911ea163cd8a8331c65993a42ecdbb56a70a
NicolasPavz <npavez43@gmail.com>
2024-11-21 18:39:32 -0300

    retroseso al commit back

--- Back/dist/src/routes/estadoRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const estadoController_1 = require("../controllers/estadoController");
-const router = (0, express_1.Router)();
-router.get('/list', estadoController_1.getEstados);
-router.get('/:id_estado', estadoController_1.getEstado);
-exports.default = router;
--- Back/tests/utils/setupTestServer.ts
+++
@@ -1,6 +0,0 @@
-import Server from '../../src/db/server';
-import supertest from 'supertest';
-
-const server = new Server();
-export const app = server['app']; 
-export const request = supertest(app); 
