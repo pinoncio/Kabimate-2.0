117d46476099cadec6fb3b721ad2a7b3379a0bab
NicolasPavz <npavez43@gmail.com>
2024-11-23 21:50:24 -0300

    agregar codigo de tests

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,48 +4,36 @@ describe('Prueba Inicio de Sesion', () => {
       cy.get('input[name="email"]').type('usuarioincorrecto@pruebas.cl')
       cy.get('input[name="contraseña"]').type('pruebas')
       cy.get('#boton').click()
-
       cy.wait(1000);
-
       cy.get('.swal2-title.custom-swal-title').should('contain', 'El email ingresado no es valido')
       cy.get('.swal2-confirm.swal2-styled').should('be.visible').click();
   })
-
 
   it('MA-0005: Inicio de sesíon incorrecta contraseña',() => {
     cy.visit('http://localhost:3000/')
       cy.get('input[name="email"]').type('usuario@pruebas.cl')
       cy.get('input[name="contraseña"]').type('contraseñaincorrecta')
       cy.get('#boton').click()
-
       cy.wait(1000);
-
       cy.get('.swal2-title.custom-swal-title').should('contain', 'Contraseña Incorrecta')
       cy.get('.swal2-confirm.swal2-styled').should('be.visible').click();
   })
-
 
   it('MA-0006: Inicio de sesíon usuario bloqueado',() => {
     cy.visit('http://localhost:3000/')
       cy.get('input[name="email"]').type('usuariobloqueado@pruebas.cl')
       cy.get('input[name="contraseña"]').type('contraseñaincorrecta')
       cy.get('#boton').click()
-
       cy.wait(1000);
-
       cy.get('.swal2-title.custom-swal-title').should('contain', 'La cuenta esta bloqueada, porfavor contacta al administrador')
       cy.get('.swal2-confirm.swal2-styled').should('be.visible').click();
   })
-
 
   it('MA-0007: Inicio de sesíon correctamente',() => {
     cy.visit('http://localhost:3000/')
       cy.get('input[name="email"]').type('usuario@pruebas.cl')
       cy.get('input[name="contraseña"]').type('pruebas')
       cy.get('#boton').click()
-
       cy.url().should('include', 'http://localhost:3000/home')
   })
-
-
 })
